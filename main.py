a238f202d9d6e95077cc7ecbb00c48ab8254cd9d
Hoor Yasin Wani <hooryasinwani@Hoor-Yasins-MacBook-Pro.local>
2026-07-06 16:48:28 -0400

    Create database tables on startup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routes.routes import router
+from app.database.database import Base, engine
+from app.models import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(router)
 
+app.include_router(router)
